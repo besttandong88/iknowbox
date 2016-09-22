49b0260514734a7dfbb739570a182edc80f3ffd5
besttandong <besttandong@126.com>
2016-09-22 11:17:59 +0800

    添加Dubbo服务

---
+++ iknowbox/iknowbox-admin-api/src/main/java/com/tandong/iknowbox/admin/api/DubboService.java
@@ -0,0 +1,26 @@
+/**
+ * <b>项目名：</b>微支付<br/>
+ * <b>包名：</b>com.tandong.iknowbox.admin.api<br/>
+ * <b>文件名：</b>DubboService.java<br/>
+ * <b>版本信息：</b><br/>
+ * <b>日期：</b>2016年9月22日-上午10:01:03<br/>
+ * <b>Copyright (c)</b> 2016 9FBank.com 玖富公司-版权所有<br/>
+ *
+ */
+package com.tandong.iknowbox.admin.api;
+
+/**
+ *
+ * DubboService
+ * 
+ * @author TanDong
+ * 2016年9月22日 上午10:01:03
+ * 
+ * @version 1.0.0
+ *
+ */
+public interface DubboService {
+	
+	public String sayHello(String name);
+
+}
